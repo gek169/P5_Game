7f32d5c0b8b7399cbcf61c0da419874dfeb44171
MHS <gek@katherine>
2021-04-27 22:42:33 -0400

    Automatic commit.

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ let entity_system;
 let renderOffset;
 const entity_max_vel = 500;
 const player_max_vel = 4;
+const debug_render_col = 1;
 
 function preload(){
 	soundeffect = loadSound('assets/click.wav');
@@ -43,9 +44,12 @@ function setup() {
   renderOffset = createVector(0,0);
   frameRate(60);
   prepImage();
-  player = new Game_Entity(createVector(100,100), 
-  						10.0, 40.0, aball, 40, 40);
-  player.isPlayer = 1;
+  entity_system = new ESystem();
+  entity_system.addEntity(createVector(100,100), 
+  						10.0, 40.0, aball, 40, 40, 1);
+
+  entity_system.addEntity(createVector(200,100), 
+    						5.0, 30.0, aball, 30, 30, 0);
 }
 
 function playSound(){
@@ -55,7 +59,7 @@ function draw() {
 /*
 Game Logic
 */
-player.integrate();
+entity_system.integrate();
 
 /*
 Rendering
@@ -69,8 +73,11 @@ Rendering
   color(255,255,255,255);
   stroke(255,255,255);
   text(url, 100, 100);
-  player.render();
+  entity_system.render();
 }
+
+
+
 
 
 //Entity class
@@ -80,7 +87,7 @@ let Game_Entity = function(position, mass, radius, sprite, spritew, spriteh){
 	this.velocity = createVector(0,0);
 	this.accel = createVector(0,0);
 	this.mass = mass;
-	this.radius = radius;
+	this.radius = radius/2;
 	this.spritew = spritew;
 	this.spriteh = spriteh;
 	this.isPlayer = 0;
@@ -100,6 +107,7 @@ Game_Entity.prototype.integrate = function(){
 		if(this.velocity.magSq() > entity_max_vel){
 			this.velocity.normalize(); this.velocity.mult(entity_max_vel);
 		}
+		this.velocity.mult(0.99); //simulate friction.
 	}
 	this.position.add(this.velocity);
 };
@@ -111,19 +119,70 @@ Game_Entity.prototype.render = function(){
 	image(this.sprite, rpos.x - this.spritew/2, rpos.y - this.spriteh/2, this.spritew, this.spriteh);
 	noStroke();
 	fill(255,255,0,100);
-	ellipse(rpos.x, rpos.y, this.radius/2.0, this.radius/2.0);
+	if(debug_render_col)
+		ellipse(rpos.x, rpos.y, this.radius*2, this.radius*2);
 };
 
 Game_Entity.prototype.collide = function(other){
 	if(this.mass <= 0 && other.mass <= 0) return 0; //Static objects do not need to be processed together.
-	let vec = this.position;
-	vec.sub(other.position);
-	let sqmag = vec.magSq();
-	if(sqmag < (other.radius + this.radius) * (other.radius + this.radius)){
-		//TODO: push on each of them.
+	let vec = other.position.copy();
+	vec.sub(this.position);
+	let sqmag = vec.mag();
+	let sumradii = (other.radius + this.radius) - sqmag;
+	if(sumradii > 0.0 && sqmag > 0){
+		vec.mult(sumradii/sqmag);
+		let mass_total = this.mass + other.mass;
+		let mass_ratio_me = this.mass / mass_total;
+		let mass_ratio_them = other.mass / mass_total;
+		if(this.mass > 0){
+			let vec2 = vec.copy();
+			vec2.mult(-1);
+			if(other.mass != 0)
+				vec2.mult(mass_ratio_them);
+			this.position.add(vec2);
+		}
+		if(other.mass > 0){
+			let vec2 = vec.copy();
+			if(this.mass != 0)
+				vec2.mult(mass_ratio_me);
+			other.position.add(vec2);
+		}
 	}
 };
 
+
+let ESystem = function(){
+	this.entities = [];
+};
+
+ESystem.prototype.addEntity = function(position, mass, radius, sprite, spritew, spriteh, isPlayer){
+	this.entities.push(new Game_Entity(position, mass, radius, sprite, spritew, spriteh))
+	this.entities[this.entities.length - 1].isPlayer = isPlayer;
+};
+
+ESystem.prototype.removeEntity = function(i){
+	this.entities.splice(i, 1);
+}
+
+ESystem.prototype.integrate = function(){
+	let i; let j;
+	for(i = this.entities.length - 1; i>-1; i--){
+		this.entities[i].integrate();
+	}
+	for(i = this.entities.length - 1; i>0; i--){
+		for(j = i-1; j>-1; j--){
+			let a = this.entities[i];
+			let b = this.entities[j];
+			a.collide(b);
+		}
+	}
+}
+
+ESystem.prototype.render = function(){
+	for(let i = this.entities.length - 1; i>=0; i--){
+		this.entities[i].render();
+	}
+}
 
 
 
