c094c821d2c481796368e23d828738d317976a62
gek169 <gek169>
2021-05-09 22:41:26 -0400

    Automatic commit.

--- sketch2.js
+++ sketch2.js
@@ -1,7 +1,30 @@
 let img;
+let world;
+let body;
+
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   img = loadImage('assets/aball.png');
+  frameRate(60);
+  world = new CANNON.World()
+  {
+  	let shape=
+  	new 
+  		CANNON.Box(new CANNON.Vec3(40, 40, 40))
+  		body = new CANNON.Body({
+  		  mass: 1,
+  		})
+  		body.addShape(shape)
+  		body.velocity.set(0,10,0);
+  		body.angularVelocity.set(20, 100, 0);
+  		body.angularDamping = 0.5;
+  		body.material = new CANNON.Material();
+  		body.material.friction = 500;
+  		world.addBody(body)
+  	}
 }
 
 function windowResized(){
@@ -9,8 +32,10 @@ function windowResized(){
 }
 
 function draw() {
+	angleMode(RADIANS);
+	world.step(1/60.0);
   background(0,0,60);
-
+	let vec3;
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
 
@@ -18,11 +43,14 @@ function draw() {
   pointLight(255, 255, 255, locX, locY, 100);
 
   push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  texture(img);
-  box(80);
+	  vec3 = new CANNON.Vec3(0,0,0);
+	  body.quaternion.toEuler(vec3);
+	  translate(body.position.x, body.position.y, body.position.z);
+	  rotateY(vec3.y);
+	  rotateZ(vec3.z);
+	  rotateX(vec3.x);
+	  texture(img);
+	  box(80);
   pop();
 
   push();
